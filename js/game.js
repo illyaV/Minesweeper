24eefa6e58debc15863e7bbf357a8343f125b07f
illyaV <illyaveklenko@gmail.com>
2015-03-02 23:20:32 +0200

    rebuilt app

---
+++ js/game.js
@@ -0,0 +1,80 @@
+/** @jsx React.DOM */
+(function(window, $, React){
+  'use strict';
+
+  var Game = React.createClass({displayName: "Game",
+    getInitialState: function(){
+      return {matrix: []};
+    },
+
+    componentDidMount: function(){
+      this.resetGame();
+    },
+
+    render: function() {
+      console.log(this.state.matrix);
+      return (React.createElement("div", {className: "game"}));
+    },
+
+    resetGame: function() {
+      var _matrix = [];
+
+      for (var i = 0; i < this.props.y; i++ ) {
+        _matrix[i] = [];
+        for (var j = 0; j < this.props.x; j++) {
+          _matrix[i][j] = 0;
+        }
+      }
+
+      this.setState({matrix: this.genTips(this.genBombs(_matrix))});
+    },
+
+    genBombs: function(_matrix) {
+      var flag = 0,
+          x, y;
+
+      while (flag <+ this.props.bombsCount) {
+        x = Math.floor(Math.random() * (this.props.x));
+        y = Math.floor(Math.random() * (this.props.y));
+        
+        if (_matrix[y][x] !== "X") {
+          _matrix[y][x] = "X";
+          flag++; 
+        }  
+      }
+
+      return _matrix;
+    },
+
+    genTips: function(_matrix) {
+      var tip;
+
+      for (var i = 0, l = _matrix.length; i < l; i++) {
+        for (var j = 0, k = _matrix[i].length; j < k; j++) {
+          if (_matrix[i][j] !== "X") {
+            tip =  0;
+
+            for (var h = i - 1; h < i+2; h++) {
+              for (var g = j  - 1; g < j + 2; g++) {
+                if (_matrix[h] && _matrix[h][g] === "X") {
+                  tip++;
+                }
+              }
+            }
+
+            if (tip > 0) {
+              _matrix[i][j] = tip;
+            } 
+          }
+        }
+      }
+
+      return _matrix; 
+    }
+  });
+
+  React.renderComponent(
+    React.createElement(Game, {x: "16", y: "16", bombsCount: "15"}),
+    document.getElementById("game")
+  );
+})(window, jQuery, React);
